dea70c65b6bbc911b8c92035db1ad3f0de119085
Loreto_menddro <loretoalmendra.rc@gmail.com>
2023-01-11 20:25:47 -0300

    boton email

--- js/email.js
+++ js/email.js
@@ -1,14 +1,17 @@
 // Enviar email cuando se presione un botón
 function sendEmail() {
+	var correo = document.getElementById ("#envio_correo");
 	Email.send({
-	Host: "smtp.gmail.com",
-	Username : "<sender’s email address>",
-	Password : "<email password>",
-	To : '<recipient’s email address>',
+	//Host: "smtp.gmail.com",
+	//Username : "<sender’s email address>",
+	//Password : "<email password>",
+	To : correo,
 	From : "<sender’s email address>",
 	Subject : "<email subject>",
 	Body : "<email body>",
 	}).then(
 		message => alert("mail sent successfully")
 	);
+	console.log("holi")
 }
+console.dir(correo)
