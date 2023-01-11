b043afc04865818bbf08c81310660ce48a289361
Cristian <ctapia.macaya@gmail.com>
2023-01-11 20:39:41 -0300

    readme modificado

--- js/email.js
+++ js/email.js
@@ -2,21 +2,12 @@
 function sendEmail() {
 	var correo = document.getElementById ("#envio_correo");
 	Email.send({
-<<<<<<< HEAD
-	Host: "smtp.gmail.com",
-	Username : "ctapia.macaya@gmail.com",
-	Password : "",
-	To : '',
-	From : "ctapia.macaya@gmail.com",
-	Subject : "pruebaa",
-=======
 	//Host: "smtp.gmail.com",
 	//Username : "<sender’s email address>",
 	//Password : "<email password>",
 	To : correo,
 	From : "<sender’s email address>",
 	Subject : "<email subject>",
->>>>>>> dea70c65b6bbc911b8c92035db1ad3f0de119085
 	Body : "<email body>",
 	}).then(
 		message => alert("mail sent successfully")
